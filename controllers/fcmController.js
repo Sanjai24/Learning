5df87cf1c7936c6fa05a84302136d628cc5fac4a
Sanjai24 <sanjaiiii24@gmail.com>
2022-07-14 15:29:14 +0530

    fcm

--- controllers/fcmController.js
+++ controllers/fcmController.js
@@ -4,8 +4,21 @@ var serverKey = require('./server_key.json');
 
 var fcm = new FCM(serverKey);
 
+const fcmController = {
+    async token_post(req, res) { 
+        try{
+            let{ token } = req.body;
+            console("token generated ----->", token);
+            return token;
+        }
+        catch(err){
+            res.send(err);
+        }
+    }
+}
+var tokencreated = fcmController.token_post;
 var message = {
-
+    to: tokencreated,
 
     notification : {
         title: 'Testing notification',
@@ -21,3 +34,5 @@ fcm.send(message, function(err, response) {
         console.log("Success ", response);
     }
 });
+
+module.exports = fcmController;
